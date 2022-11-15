de60b1dc71df2e0ab02374e60d2bc3911ce62d45
Noskcaj <kcajdev@gmail.com>
2022-11-15 12:53:37 -0500

    Tentative fix for fzf-tab partial matching

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -3,3 +3,15 @@
 
 export FZF_DEFAULT_COMMAND="rg --files"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+## Fix fzf-tab for partial matches with multiple candidates
+fzf-tab-partial-and-complete() {
+    if [[ $LASTWIDGET = 'fzf-tab-partial-and-complete' ]]; then
+        fzf-tab-complete
+    else
+        zle complete-word
+    fi
+}
+
+zle -N fzf-tab-partial-and-complete
+bindkey '^I' fzf-tab-partial-and-complete
